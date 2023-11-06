4bb14ae46859a4005847768dcca2a534cd90b8cb
Rohan Srinivasan <nimbus.series@gmail.com>
2023-11-06 11:49:09 +0530

    In progress: Locking

--- TCPServer.java
+++ TCPServer.java
@@ -1,23 +1,26 @@
 import java.io.*;
 import java.net.*;
 import java.util.StringTokenizer;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Server {
     private static final int BUFFERSIZE = 1024;
     private static final int MAXPENDING = 10;
 
+    // lock
+    private static ReentrantLock lock = null;
     public static void main(String[] args) {
         ServerSocket serverSocket = null;
 
         try {
             serverSocket = new ServerSocket(12345); // Port number can be changed
             System.out.println("Server Socket Created");
-
+            lock = new ReentrantLock(true);
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected: " + clientSocket);
 
-                ClientHandler clientThread = new ClientHandler(clientSocket);
+                ClientHandler clientThread = new ClientHandler(clientSocket, lock);
                 clientThread.start();
             }
         } catch (IOException e) {
@@ -36,9 +39,12 @@ public class Server {
 
 class ClientHandler extends Thread {
     private final Socket clientSocket;
+    // lock
+    private static ReentrantLock lock = null;
 
-    public ClientHandler(Socket socket) {
+    public ClientHandler(Socket socket, ReentrantLock lock) {
         this.clientSocket = socket;
+        this.lock = lock;
     }
 
     @Override
@@ -53,8 +59,13 @@ class ClientHandler extends Thread {
                     System.out.println("Client disconnected: " + clientSocket);
                     break;
                 } else {
-                    String response = processRequest(msg);
-                    output.println(response);
+                    lock.lock();
+                    try {
+                        String response = processRequest(msg);
+                        output.println(response);
+                    } finally {
+                        lock.unlock();
+                    }
                 }
             }
 
@@ -65,8 +76,49 @@ class ClientHandler extends Thread {
     }
 
     private String processRequest(String msg) {
-        String response;
-        // Open file database.txt
-        return "Processed: " + msg;
+        String response = "";
+        String[] parts = msg.split(" ");
+        String req = parts[0];
+
+        if (req.equals("put")) {
+            if (parts.length >= 3) {
+                int key = Integer.parseInt(parts[1]);
+                String value = parts[2];
+
+                try {
+                    // Open the file for reading as well as writing
+                    RandomAccessFile file = new RandomAccessFile("data.txt", "rw");
+                    String line;
+                    boolean found = false;
+                    while ((line = file.readLine()) != null) {
+                        String[] lineParts = line.split(" ");
+                        int lineKey = Integer.parseInt(lineParts[0]);
+                        if (lineKey == key) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found) {
+                        // Update the value
+                        file.seek(file.getFilePointer() - line.length() - 1);
+                        file.writeBytes(key + " " + value + "\n");
+                        response = "Updated value for key " + key;
+                    } else {
+                        // Append the new key-value pair
+                        file.seek(file.length());
+                        file.writeBytes(key + " " + value + "\n");
+                        response = "Added new key-value pair";
+                    }
+                    file.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    response = "Error while processing request";
+                }
+            } else {
+                response = "Invalid request";
+            }
+        }
+
+        return response;
     }
 }
